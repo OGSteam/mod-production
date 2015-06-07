c9a2d4c74474cd3a0adbfea955f4c726ba341302
Allegre <pitch314@users.noreply.github.com>
2015-06-07 11:46:48 +0200

    Conversin UTF-8

--- lang/lang_fr.php
+++ lang/lang_fr.php
@@ -3,7 +3,7 @@
 *	filename	: lang_fr.php
 *	package		: Mod Production
 *	version		: 1.4
-*	desc.			: Liste des chaines pour la langue Française.
+*	desc.			: Liste des chaines pour la langue FranÃ§aise.
 *	Authors		: Scaler - http://ogsteam.fr
 *	created		: 26/08/2008
 *	modified	: 10:34 05/09/2009
@@ -16,37 +16,37 @@ if (!defined('IN_SPYOGAME')) die("Hacking attempt");
 $lang['prod_uction'] = "Production";
 $lang['prod_uction_mod'] = "Mod Production";
 $lang['prod_save'] = "Sauvegarder";
-$lang['prod_reset'] = "Restaurer les données";
-$lang['prod_planete'] = "Planète";
-$lang['prod_coordinates'] = "Coordonnées";
-$lang['prod_temperature'] = "Température maximale";
+$lang['prod_reset'] = "Restaurer les donnÃ©es";
+$lang['prod_planete'] = "PlanÃ¨te";
+$lang['prod_coordinates'] = "CoordonnÃ©es";
+$lang['prod_temperature'] = "TempÃ©rature maximale";
 $lang['prod_fields'] = "Cases";
-$lang['prod_energy'] = "Energie";
-$lang['prod_created_by'] = "Mod Production v1.4.a developpéé par "./*$creator_name*/"%2\$s modifié par "./*$modifier_name1*/"%3\$s, "./*$modifier_name2*/"%4\$s et "./*$modifier_name3*/"%5\$s.";
-$lang['prod_updated_by'] = "Mod Production v"./*$mod_version*/"%s Mise à jour par "./*$updator_name*/"%2\$s";
+$lang['prod_energy'] = "Ã‰nergie";
+$lang['prod_created_by'] = "Mod Production v1.4.a dÃ©veloppÃ©e par "./*$creator_name*/"%2\$s modifiÃ© par "./*$modifier_name1*/"%3\$s, "./*$modifier_name2*/"%4\$s et "./*$modifier_name3*/"%5\$s.";
+$lang['prod_updated_by'] = "Mod Production v"./*$mod_version*/"%s Mise Ã  jour par "./*$updator_name*/"%2\$s";
 $lang['prod_forum'] = "plus d'informations";
 
 // Buildings
-$lang['prod_buildings'] = "Bâtiments";
-$lang['prod_building_M'] = "Mine de métal";
+$lang['prod_buildings'] = "BÃ¢timents";
+$lang['prod_building_M'] = "Mine de mÃ©tal";
 $lang['prod_building_C'] = "Mine de cristal";
-$lang['prod_building_D'] = "Synthétiseur de deutérium";
-$lang['prod_building_SoP'] = "Centrale électrique solaire";
-$lang['prod_building_FR'] = "Centrale électrique de fusion";
+$lang['prod_building_D'] = "SynthÃ©tiseur de deutÃ©rium";
+$lang['prod_building_SoP'] = "Centrale Ã©lectrique solaire";
+$lang['prod_building_FR'] = "Centrale Ã©lectrique de fusion";
 
 // Solar Satellite
 $lang['prod_SS'] = "Satellite solaire";
 
 // Technologies and Officers
 $lang['prod_tech_off'] = "Technologies et Officiers";
-$lang['prod_technology_En'] = "Technologie Energie";
+$lang['prod_technology_En'] = "Technologie Ã‰nergie";
 $lang['prod_technology_Pl'] = "Techno Plasma";
-$lang['prod_officer_E'] = "Officier Ingénieur";
-$lang['prod_officer_E_help'] = "+10% d\'énergie";
-$lang['prod_officer_G'] = "Officier Géologue";
+$lang['prod_officer_E'] = "Officier IngÃ©nieur";
+$lang['prod_officer_E_help'] = "+10% d\'Ã©nergie";
+$lang['prod_officer_G'] = "Officier GÃ©ologue";
 $lang['prod_officer_G_help'] = "+10% de production";
 $lang['prod_officer_full'] = "Full officiers";
-$lang['prod_officer_full_help'] = "+12% de production et d\'énergie";
+$lang['prod_officer_full_help'] = "+12% de production et d\'Ã©nergie";
 
 // Production
 $lang['prod_prod_hour'] = "Production par heure";
@@ -54,16 +54,16 @@ $lang['prod_prod_day'] = "Production par jour";
 $lang['prod_prod_factor'] = "Facteur de production";
 $lang['prod_total_prod'] = "Production globale";
 $lang['prod_account'] = "Prendre en compte";
-$lang['prod_none'] = "Cliquez pour désélectionner toutes les planètes";
-$lang['prod_all'] = "Cliquez pour sélectionner toutes les planètes";
+$lang['prod_none'] = "Cliquez pour dÃ©-sÃ©lectionner toutes les planÃ¨tes";
+$lang['prod_all'] = "Cliquez pour sÃ©lectionner toutes les planÃ¨tes";
 $lang['prod_hour'] = "Heure";
 $lang['prod_day'] = "Jour";
 $lang['prod_week'] = "Semaine";
-$lang['prod_M'] = "Métal";
+$lang['prod_M'] = "MÃ©tal";
 $lang['prod_C'] = "Cristal";
-$lang['prod_D'] = "Deutérium";
+$lang['prod_D'] = "DeutÃ©rium";
 $lang['prod_points'] = "Points";
 $lang['prod_ratio'] = "Rapports";
-$lang['prod_ref'] = "Cliquez pour utiliser comme référence";
+$lang['prod_ref'] = "Cliquez pour utiliser comme rÃ©fÃ©rence";
 ?>
 
