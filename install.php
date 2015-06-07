1886a2efc815d3da3d2fd204ea892a4d5ee2cf8c
Allegre <pitch314@users.noreply.github.com>
2015-06-07 11:40:34 +0200

    Correction utilisation BDD avec $db

--- install.php
+++ install.php
@@ -7,13 +7,13 @@ $is_ok = install_mod($mod_folder);
 if ($is_ok == true)
 	{
 		// Vérification de la présence des informations officiers et insertion si besoin (OGSpy < 3.05)
-		$quet = mysql_query("SELECT * FROM ".TABLE_USER." WHERE `off_amiral` = '0' OR `off_amiral` = '1' LIMIT 1");
+		$quet = $db->sql_query("SELECT * FROM ".TABLE_USER." WHERE `off_amiral` = '0' OR `off_amiral` = '1' LIMIT 1");
 		if (!$quet) 
 			{
-				$query = "ALTER TABLE ".TABLE_USER." ADD `off_amiral` ENUM( '0', '1' ) NOT NULL DEFAULT '0',
-					ADD `off_ingenieur` ENUM( '0', '1' ) NOT NULL DEFAULT '0',
-					ADD `off_geologue` ENUM( '0', '1' ) NOT NULL DEFAULT '0',
-					ADD `off_technocrate` ENUM( '0', '1' ) NOT NULL DEFAULT '0'";
+				$query = "ALTER TABLE ".TABLE_USER." ADD COLUMN `off_amiral` ENUM( '0', '1' ) NOT NULL DEFAULT '0',
+					ADD COLUMN `off_ingenieur` ENUM( '0', '1' ) NOT NULL DEFAULT '0',
+					ADD COLUMN `off_geologue` ENUM( '0', '1' ) NOT NULL DEFAULT '0',
+					ADD COLUMN `off_technocrate` ENUM( '0', '1' ) NOT NULL DEFAULT '0'";
 				$db->sql_query($query);
 			}
 	}
